cf9f3a4a7eb3d300e16d223344d91222f8112669
MallarChanda <85064012+MallarChanda@users.noreply.github.com>
2021-10-18 17:11:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload() {   
+}
+
+function setup() {
+canvas = createCanvas(300,300);
+canvas.center();   
+}
+
+function draw() {
+    
+}
+
+function takesnapshot() {
+save('my_mustached_image.png');   
+}
